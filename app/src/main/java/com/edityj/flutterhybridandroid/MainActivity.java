ea9a436998e933bb507a9a67bf32f983e47c90a1
EditYJ <158392613@qq.com>
2019-09-26 10:52:33 +0800

    利用Fragment嵌入flutter应用

--- app/src/main/java/com/edityj/flutterhybridandroid/MainActivity.java
+++ app/src/main/java/com/edityj/flutterhybridandroid/MainActivity.java
@@ -1,8 +1,12 @@
 package com.edityj.flutterhybridandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import io.flutter.facade.Flutter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+                tx.replace(R.id.someContainer,
+                        Flutter.createFragment("{name:'俞杰', dataList:['aa','bb','cc']}"));
+                tx.commit();
+            }
+        });
     }
 }
